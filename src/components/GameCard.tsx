d0a1e8a1ac4091c2043457fce7e3c02dc93e8850
CS\Randika <subash.rodrigo@gmail.com>
2023-09-29 23:31:12 +0530

    Fixed minor issues with game card and games grid.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,15 +14,15 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading size={{ sm: "lg", md: "md", lg: "md", xl: "md" }}>
-          {game.name}
-        </Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((pp) => pp.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading size={{ sm: "lg", md: "md", lg: "md", xl: "md" }}>
+          {game.name}
+        </Heading>
       </CardBody>
     </Card>
   );
