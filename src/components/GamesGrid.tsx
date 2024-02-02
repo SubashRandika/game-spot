f525d04f1acfddedaa1c38bfe997d66b6388f737
SubashRandika <subash.rodrigo@gmail.com>
2024-02-02 21:33:33 +0530

    Implemented infinite scrolling for games grid using react-infinite-scroll-component library.

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,33 +1,36 @@
-import { Button, Center, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { GameQuery } from "../App";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import { GameQuery } from "../App";
-import useGames from "../hooks/useGames";
-import React from "react";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GamesGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    fetchNextPage,
-    isFetchingNextPage,
-    hasNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
+    useGames(gameQuery);
   const skeletons = Array.of(1, 2, 3, 4, 5, 6, 7, 8);
 
   if (error) {
     return <Text>{error.message}</Text>;
   }
 
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
-    <>
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      next={() => fetchNextPage()}
+      hasMore={!!hasNextPage}
+      loader={<Spinner />}
+    >
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6} padding={10}>
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -45,19 +48,7 @@ const GamesGrid = ({ gameQuery }: Props) => {
           </React.Fragment>
         ))}
       </SimpleGrid>
-      {hasNextPage && (
-        <Center>
-          <Button
-            marginBottom={5}
-            isLoading={isFetchingNextPage}
-            loadingText="Loading more..."
-            onClick={() => fetchNextPage()}
-          >
-            Load more
-          </Button>
-        </Center>
-      )}
-    </>
+    </InfiniteScroll>
   );
 };
 
