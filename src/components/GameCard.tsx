c91bc3fd2360b24062bf354605dd216829dd7e9c
Subash Rodrigo <subash.rodrigo@gmail.com>
2024-08-31 01:52:21 +0530

    Refactored interfaces by extracting as entities.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,7 @@ import Emoji from "./Emoji";
 import getCroppedImageUrl from "../services/image-url";
 import PlatformIconList from "./PlatformIconList";
 import { Card, CardBody, Flex, Heading, HStack, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { Link } from "react-router-dom";
 
 interface Props {
